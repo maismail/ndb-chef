e4a058c28eedb662a250e4c26ebc3e6760eebb79
Mahmoud Ismail <mahmoud@logicalclocks.com>
2020-05-12 17:06:41 +0200

    add extra recipe to run bench nodes

---
+++ recipes/_bench_node.rb
@@ -0,0 +1,10 @@
+homedir = node['ndb']['user'].eql?("root") ? "/root" : "/home/#{node['ndb']['user']}"
+
+# Add the mgmd hosts' public key, so that it can start/stop the ndbd on this node using passwordless ssh.
+kagent_keys "#{homedir}" do
+  cb_user "#{node['ndb']['user']}"
+  cb_group "#{node['ndb']['group']}"
+  cb_name "ndb"
+  cb_recipe "mgmd"
+  action :get_publickey
+end
